441480283570c6e58e0d563ba22dd73d97d19feb
Danilo <damad@fi.uba.ar>
2020-12-20 11:12:39 -0300

    Borrado, solo servia para hacer referencias circulares

--- src/globales.h
+++
@@ -1,7 +0,0 @@
-#ifndef GLOBALES
-#define GLOBALES
-#include "ListaDeGenes.h"
-#include <string>
-extern ListaDeGenes listaDeGenes; //todos los que incluyan a este archivo van a poder usar esta instancia
-#endif
-
