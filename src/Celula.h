8ff9ebaf6944ba4716285f156aa197276867239e
Danilo <damad@fi.uba.ar>
2020-12-20 11:14:52 -0300

    Ahora la celula empieza muerta por default, agregada la funcion agregarGen()

--- src/Celula.h
+++ src/Celula.h
@@ -18,12 +18,16 @@ public:
 	*/
 	std::vector<InformacionYCarga*> getGenes();
 	/*
+	post: la celula está viva, la celula tiene el gen que se agrega
+	*/
+	void agregarGen(InformacionYCarga * gen);
+	/*
 	post: la celula es el producto de la reproduccion de las tres celulas padres
 	*/
 	Celula(Celula* padre1, Celula* padre2, Celula* padre3);
 	/*
 	post: this->genes contiene punteros a los genes del vector que se pasa por agumento
 	*/
-	Celula(std::vector<InformacionYCarga*> genes);
+	Celula();
 };
 #endif
