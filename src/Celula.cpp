aa1aa79e5ef1cc8586eddb455a1a16178a04a0be
Danilo <damad@fi.uba.ar>
2020-12-20 11:13:42 -0300

    Ahora la celula empieza muerta por default, agregada la funcion agregarGen()

--- src/Celula.cpp
+++ src/Celula.cpp
@@ -10,13 +10,19 @@ std::vector<InformacionYCarga*> Celula::getGenes()
 	return genes;
 }
 
+void Celula::agregarGen(InformacionYCarga* info)
+{
+	this->estaViva = true;
+	this->genes.push_back(info);
+}
+
 Celula::Celula(Celula* padre1, Celula* padre2, Celula* padre3)
 {
 	estaViva = true;
 }
 
-Celula::Celula(std::vector<InformacionYCarga*> genes)
+Celula::Celula()
 {
-	estaViva = true;
-	this->genes = genes;
+	estaViva = false;
+	this->genes = {};
 }
