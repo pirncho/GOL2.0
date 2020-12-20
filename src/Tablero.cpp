1208630c1cc5bd6e6bb1e512dbbe7ced7fbcfa34
Danilo <damad@fi.uba.ar>
2020-12-20 11:17:23 -0300

    Terminanado el constructor

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -20,13 +20,51 @@ int* Tablero::getDimensiones()
 Tablero::Tablero(std::string rutaArchivo)
 {
 	Archivo archivo(rutaArchivo);
-	std::vector<std::string>* linea;
+	std::vector<std::string>* linea = nullptr;
 	do
 	{
 		linea = archivo.getLinea();
-		if ((*linea)[0] == "Tablero") {
+		int x = 0, y = 0; // variables que van a guardar la posicion de la celula
+		Celula* celula = nullptr; // variable temporal que va a guardar una celula mientras la creo
+		/*
+		* la linea que empieza con "tablero" es la primera, que indica el tamaño
+		*/
+		if ((*linea)[0] == "tablero") {
 			this->alto = std::stoi((*linea)[1]);
 			this->ancho = std::stoi((*linea)[2]);
+			this->tablero = new Celula**[alto];
+			for (int i = 0; i < alto; i++) {
+				this->tablero[i] = new Celula * [ancho];
+			}
 		}
-	} while (linea);
+		/*
+		* la linea que empieza con "celula" indica la descripcion de una nueva celula
+		*/
+		if ((*linea)[0] == "celula") {
+			celula = new Celula;
+		}
+		/*
+		* la linea que empieza con "gen" indica un gen para agregar a la celulas
+		*/
+		if ((*linea)[0] == "gen") {
+			InformacionGenetica bits((*linea)[1]);
+			InformacionYCarga* info = new InformacionYCarga(bits ,std::stoi((*linea)[2]));
+			celula->agregarGen(info);
+		}
+		/*
+		* la linea que dice "fin" indica el fin de la descripicion de una celula
+		*/
+		if ((*linea)[0] == "fin") {
+			tablero[y][x] = celula;
+		}
+
+		} while (linea);
+}
+
+Tablero::Tablero()
+{
+	this->alto = 0;
+	this->ancho = 0;
+	this->edad = 0;
+	this->tablero = nullptr;
 }
